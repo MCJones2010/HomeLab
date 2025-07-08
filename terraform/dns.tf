635928165cd67fbf9940d4a2c222dee4f8343455
Matt Jones <MCJones2010@gmail.com>
2025-07-07 18:17:53 -0600

    Terraform build out completed, moving to initialization

---
+++ terraform/dns.tf
@@ -0,0 +1,8 @@
+resourece "cloudflare_record" "homelab" {
+  zone_id = var.cf_zone_id
+  name    = "@"
+  type    = "A"
+  value   = var.wan_ip
+  ttl     = 300
+  proxied = true
+}
---
+++ terraform/outputs.tf
@@ -0,0 +1,2 @@
+output "record_id" { value = cloudflare_record.homelab.id }
+output "zone_id"   { value = cloudflare_record.homelab.zone_id }
