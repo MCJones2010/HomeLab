73a83ca4bdf4438ea383148172c4880cd4d46fee
Matt Jones <MCJones2010@gmail.com>
2025-07-07 18:04:13 -0600

    wrapping up ansible, moving to terraform

---
+++ terraform/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+provider "cloudflare" {
+  email   = var.cf_email
+  api_key = var.cf_api_key
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "cf_email"    { type = string }
+variable "cf_api_key"  { type = string, sensitive = true }
+variable "cf_zone_id"  { type = string }
+variable "wan_ip"      { type = string }
